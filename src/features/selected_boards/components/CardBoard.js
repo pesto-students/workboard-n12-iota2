350dfae132acb7a9b60b9010e27b0d6e8444f8fe
snapshahab <shahabsyed091@gmail.com>
2022-02-05 16:28:41 +0530

    removed unused comments

--- src/features/selected_boards/components/CardBoard.js
+++ src/features/selected_boards/components/CardBoard.js
@@ -29,36 +29,6 @@ export default function CardBoard({
       if (draggingItem.id !== id) {
         moveCard(draggingItem.id, stageId, index);
       }
-      // if (!ref.current) {
-      //   return;
-      // }
-      // const dragIndex = item.index;
-      // const hoverIndex = index;
-      // // Don't replace items with themselves
-      // if (dragIndex === hoverIndex) {
-      //   return;
-      // }
-      // // Determine rectangle on screen
-      // const hoverBoundingRect = ref.current?.getBoundingClientRect();
-      // // Get vertical middle
-      // const hoverMiddleY =
-      //   (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-      // // Determine mouse position
-      // const clientOffset = monitor.getClientOffset();
-      // // Get pixels to the top
-      // const hoverClientY = clientOffset.y - hoverBoundingRect.top;
-      // // Only perform the move when the mouse has crossed half of the items height
-      // // When dragging downwards, only move when the cursor is below 50%
-      // // When dragging upwards, only move when the cursor is above 50%
-      // // Dragging downwards
-      // if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-      //   return;
-      // }
-      // if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-      //   return;
-      // }
-      // moveCard(dragIndex, hoverIndex);
-      // item.index = hoverIndex;
     },
   });
   const [, drag] = useDrag({
